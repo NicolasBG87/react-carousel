04e1d7d46fe591b05456351e2f936c2ca59ee2e1
Nikola Bojanovic <nikolabojanovicmob@gmail.com>
2019-04-10 13:39:26 +0200

    Updates

---
+++ precache-manifest.247b2bc689f3077d524b16e7d2d01f80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff19c07fe8af170675b776342cff3ee0",
+    "url": "/react-carousel/static/media/img-3.ff19c07f.jpg"
+  },
+  {
+    "revision": "7eac0a715644dd997b4af573e72a5824",
+    "url": "/react-carousel/static/media/img-2.7eac0a71.jpg"
+  },
+  {
+    "revision": "ca6306a419a6b07ee9edde1c6169500e",
+    "url": "/react-carousel/static/media/img-1.ca6306a4.jpg"
+  },
+  {
+    "revision": "7f7feaa79c4ebfe5916f",
+    "url": "/react-carousel/static/js/runtime~main.9f3c6066.js"
+  },
+  {
+    "revision": "ca9225fd3e15a8950c2d",
+    "url": "/react-carousel/static/js/main.97217938.chunk.js"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/react-carousel/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "ca9225fd3e15a8950c2d",
+    "url": "/react-carousel/static/css/main.185eb143.chunk.css"
+  },
+  {
+    "revision": "abba9e325b79a0a865d9325e8cae0045",
+    "url": "/react-carousel/index.html"
+  }
+];
